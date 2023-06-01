07f4870cef6ef207104b9ab305912c7c8e8378ff
Yuri Hartmann <yurihartmann0607@gmail.com>
2023-06-01 00:04:04 +0000

    👨🏻‍💻

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import time
+from numpy.random import rand
+
+print(f"Iniciando processamento com {os.getenv('OMP_NUM_THREADS')} Thread(s) ...")
+
+times = []
+
+
+def average(lst):
+    return sum(lst) / len(lst)
+
+
+for i in range(5):
+    # size of arrays
+    n = 8000
+    # create an array of random values
+    data1 = rand(n, n)
+    data2 = rand(n, n)
+
+    start = time.time()
+
+    # run
+    data1.dot(data2)
+
+    end = time.time()
+    times.append(end - start)
+    print(f'Rodando {i+1} vez Tempo: {end - start} sec')
+
+print("Tempo médio: ", average(times), "sec")
---
+++ run.sh
@@ -0,0 +1,7 @@
+export OMP_NUM_THREADS=1
+
+python ./main.py
+
+OMP_NUM_THREADS=8
+
+python ./main.py
